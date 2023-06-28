87fecee973671b1627866af82e5c8ebc102483ce
Manoel-I <manoel.122112@outlook.com>
2023-06-28 18:40:16 -0300

    JWT implementation

--- APIDeGames/index.js
+++ APIDeGames/index.js
@@ -2,12 +2,31 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+
+const JWTSecret = "segredoDoManoel";
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+function auth(req, res, next){
+    const authToken = req.headers['authorization'];
+    let bearer_array = authToken.split(' ');
+    let token = bearer_array[1];
+
+    jwt.verify(token, JWTSecret, (error, data)=>{
+        if(error){
+            send.status(401);
+            send.json({error : "Invalid token"});
+        }else{
+            req.token = token;
+            req.loggedUser = {id : data.id, email : data.email};
+            next();
+        }
+    });
+}
 
 var DB = {
     games: [
@@ -29,10 +48,18 @@ var DB = {
             year: 2012,
             price: 20
         }
+    ],
+    users:[
+        {
+            id: 1,
+            name: "manoel",
+            email: "manoel.bcneto@gmail.com.br",
+            password: "Manu333opaNode"
+        }
     ]
 }
 
-app.get("/games",(req, res) => {
+app.get("/games",auth,(req, res) => {
     res.statusCode = 200;
     res.json(DB.games);
 });
@@ -121,6 +148,39 @@ app.put("/game/:id",(req, res) => {
     }
 
 });
+
+app.post('/auth', (req, res)=>{
+    let {email, password} = req.body;
+
+    if(email != undefined){
+        let user = DB.users.find(user => user.email == email);
+        if(user != undefined){
+            if(user.password == password){
+
+                // payload do token 
+                jwt.sign({id : user.id, email: user.email},JWTSecret,{expiresIn: '1h'}, (error, token)=>{   
+                    if(error){
+                        res.status(400);
+                        res.json({error : "internal failure"});
+                    }else{
+                        res.status(200);
+                        res.json({token : token });
+                    }
+                }); 
+            }else{
+                res.status(401);
+                res.json({error : "invalid password"});
+            }
+        }else{
+            res.status(404);
+            res.json({error: "user not found"});
+        }
+    }else{
+        res.status(400);
+        res.json({error : "invalid email"})
+    }
+})
+
 
 app.listen(8080,() => {
     console.log("API RODANDO!");
